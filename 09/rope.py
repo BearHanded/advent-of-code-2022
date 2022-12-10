6e54f9d6708a88b5049a18c11a3ff79a247cbbac
bearHanded <bcm3223@gmail.com>
2022-12-10 09:55:40 -0500

    9 and 10 written for concurrent. oops

--- 09/rope.py
+++ 09/rope.py
@@ -2,6 +2,7 @@ from util import christmas_input
 
 INPUT = [[i for i in line.split(" ")] for line in christmas_input.file_to_array('input.txt')]
 TEST_INPUT = [[i for i in line.split(" ")] for line in christmas_input.file_to_array('test_input.txt')]
+TEST_INPUT_2 = [[i for i in line.split(" ")] for line in christmas_input.file_to_array('test_input_2.txt')]
 
 DIRECTIONS = {
     "R": (1, 0),
@@ -11,31 +12,35 @@ DIRECTIONS = {
 }
 
 
-def total_tail_positions(orders):
-    head = (0, 0)
-    tail = [0, 0]
+def total_tail_positions(orders, rope_length=2):
+    segments = [[0, 0] for _ in range(rope_length)]
     tail_locations = set()
 
-    for orders in orders:
-        ray = DIRECTIONS[orders[0]]
-        for _ in range(int(orders[1])):
-            head = (head[0] + ray[0], head[1] + ray[1])
-            diff_x = head[0] - tail[0]
-            diff_y = head[1] - tail[1]
-            if abs(diff_x) > 1:
-                tail[0] += 1 if diff_x > 0 else -1
-                if head[1] != tail[1]:
-                    tail[1] = head[1]
-            elif abs(diff_y) > 1:
-                tail[1] += 1 if diff_y > 0 else -1
-                if head[0] != tail[0]:
-                    tail[0] = head[0]
+    for order in orders:
+        ray = DIRECTIONS[order[0]]
+        for _ in range(int(order[1])):
+            segments[0] = [segments[0][0] + ray[0], segments[0][1] + ray[1]]
+            for i, current in enumerate(segments[1:]):
+                # Note segment[i] is the prior segment, as the iteration starts at +1
+                diff_x = segments[i][0] - current[0]
+                diff_y = segments[i][1] - current[1]
 
-            tail_locations.add((tail[0],tail[1]))
-            print(head, "-", tail)
-    print(len(tail_locations), tail_locations)
+                if abs(diff_y) > 1:
+                    current[1] += (1 if diff_y > 0 else -1)
+                    if diff_x != 0:
+                        current[0] += (1 if diff_x > 0 else -1)
+                elif abs(diff_x) > 1:
+                    current[0] += (1 if diff_x > 0 else -1)
+                    if diff_y != 0:
+                        current[1] += (1 if diff_y > 0 else -1)
+                if i == (rope_length - 2):
+                    tail_locations.add((current[0], current[1]))
     return len(tail_locations)
 
 
 assert total_tail_positions(TEST_INPUT) == 13
+assert total_tail_positions(TEST_INPUT, 10) == 1
+assert total_tail_positions(TEST_INPUT_2, 10) == 36
+
 print("PART ONE: ", total_tail_positions(INPUT))
+print("PART ONE: ", total_tail_positions(INPUT, 10))
---
+++ 10/tube.py
@@ -0,0 +1,45 @@
+from util import christmas_input
+
+INPUT = [[i for i in line.split(" ")] for line in christmas_input.file_to_array('input.txt')]
+TEST_INPUT = [[i for i in line.split(" ")] for line in christmas_input.file_to_array('test_input.txt')]
+
+CYCLE_TIMING = {
+    "noop": 0,
+    "addx": 1
+}
+
+
+def queue_insert(queue, position, value):
+    size = len(queue)
+    if len(queue) < position + 1:
+        queue.extend([] for _ in range(size, position + 1))
+    queue[position].append(value)
+
+
+def signal_strength(commands):
+    cycle = 1
+    signal_str = 0
+    queue = []
+    x = 1
+    while len(queue) > 0 or cycle <= len(commands):
+        # Set queue
+        if cycle <= len(commands):
+            op = commands[cycle - 1]
+            queue_insert(queue, CYCLE_TIMING[op[0]], op)
+
+        # Process
+        current_ops = queue.pop(0);
+        for executing_op in current_ops:
+            # if executing_op[0] == "noop":
+            if executing_op[0] == "addx":
+                x += int(executing_op[1])
+        if cycle == 20 or (cycle - 20) % 40 == 0:
+            signal_str += cycle * x
+            print("  ", cycle, x)
+            print("SIGNAL STR -", cycle, signal_str)
+        cycle += 1
+    return signal_str
+
+
+assert signal_strength(TEST_INPUT) == 13140
+print("Part One: ", signal_strength(INPUT))
