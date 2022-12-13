c664aa57df783ae45d48f3424dc4601b933c9f7b
bearHanded <bcm3223@gmail.com>
2022-12-13 11:24:38 -0500

    done 13

--- 13/signal.py
+++ 13/signal.py
@@ -1,11 +1,11 @@
 from util import christmas_input
+from functools import cmp_to_key
 
 INPUT = [[eval(j) for j in i] for i in christmas_input.file_to_subarray('input.txt')]
 TEST_INPUT = [[eval(j) for j in i] for i in christmas_input.file_to_subarray('test_input.txt')]
 
 
 def check_order(left, right):
-    print("  ", left, right)
     if type(left) == list and type(right) == list:
         search_pair = (left, right)
     elif type(left) == list and type(right) == int:
@@ -14,36 +14,45 @@ def check_order(left, right):
         search_pair = ([left], right)
     else:
         if left == right:   # No decision possible
-            return None
-        print("      ", left < right)
-        return left < right
+            return 0
+        return 1 if left < right else -1
     # Recursion
     left_list, right_list = search_pair
     for i in range(min(len(left_list), len(right_list))):
         result = check_order(left_list[i], right_list[i])
-        print("            results", result)
-        if result is not None:
+        if result is not 0:
             return result
 
-    print("          ", search_pair)
     if len(left_list) == len(right_list):  # Right side ran out
-        return None
+        return 0
     else:
-        return len(left_list) < len(right_list)
+        return 1 if len(left_list) < len(right_list) else -1
 
 
 def verification_value(data):
     correct_indices = []
     for i, pair in enumerate(data):
-        print("PAIR ", i + 1,"\n")
         a, b = pair[0], pair[1]
-        if check_order(a, b) is not False:  # Can be none for a non-determination
+        if check_order(a, b) >= 0:  # Can be none for a non-determination
             correct_indices.append(i + 1)
-            print(correct_indices)
-    print("Correct", correct_indices, sum(correct_indices))
     return sum(correct_indices)
 
 
-# assert(verification_value([[[[5, 5], 1], [[5], 5]]]) == 0)
+def decoder_key(f):
+    signals = [eval(i) for i in christmas_input.file_to_array(f) if i]
+    markers = [[[2]], [[6]]]
+    signals.extend(markers)
+    signals.sort(key=cmp_to_key(check_order))
+    signals.reverse()  # backwards because of -1 vs 1, oops
+
+    key = 1
+    for i, signal in enumerate(signals):
+        if signal in markers:
+            key *= i + 1
+    return key
+
+
 assert verification_value(TEST_INPUT) == 13
 print("Part One: ", verification_value(INPUT))
+assert decoder_key("test_input.txt") == 140
+print("Part TWO: ", decoder_key("input.txt"))
