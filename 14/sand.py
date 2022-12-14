f10cd92efba917ae132917ff0edce3b0cef6353a
bearHanded <bcm3223@gmail.com>
2022-12-14 10:35:39 -0500

    sandy code

--- 14/sand.py
+++ 14/sand.py
@@ -1,5 +1,4 @@
 from util import christmas_input
-from functools import cmp_to_key
 
 INPUT = 'input.txt'
 TEST_INPUT = 'test_input.txt'
@@ -20,25 +19,28 @@ def build_map(filename):
             y_dir = -1 if (a[1] - b[1]) >= 0 else 1
             for x in range(magnitude_x + 1):
                 for y in range(magnitude_y + 1):
-                    point = (a[0] + x * x_dir, a[1] + y * y_dir)
                     # Since only horizontal & vert, this is safe
+                    point = (a[0] + x * x_dir, a[1] + y * y_dir)
                     stone_set.add(point)
                     if point[1] > max_y:
                         max_y = point[1]
-    print(lines)
-    print(stone_set)
     return stone_set, max_y
 
 
-def simulate_sand(stone_set, max_y):
+def simulate_sand(stone_set, max_y, floor=None):
     settled = False
     x, y = SAND_START
     y_out = max_y
     while not settled and y <= max_y:
         # Keep falling if avail
-        if (x, y + 1) not in stone_set:
+        if floor and y == (floor - 1):
+            settled = True
+            stone_set.add((x, y))
+            break
+        elif (x, y + 1) not in stone_set:
             y += 1
             continue
+
         # collision, check if side scatter
         if (x - 1, y + 1) not in stone_set:
             x -= 1
@@ -48,17 +50,16 @@ def simulate_sand(stone_set, max_y):
             x += 1
             y += 1
             continue
-        # TODO: Find new valid coordinate, and move sand there. continue.
+
         settled = True
         stone_set.add((x, y))
-        print((x,y))
         if y > max_y:
             y_out = max_y if max_y > y else y
 
     return settled, y_out
 
 
-def max_moves(filename):
+def max_moves_void(filename):
     stone_set, max_y = build_map(filename)
     running = True
     total_sand = 0
@@ -69,5 +70,19 @@ def max_moves(filename):
     return total_sand - 1
 
 
-assert max_moves(TEST_INPUT) == 24
-print("Part One: ", max_moves(INPUT))
+def max_moves_floor(filename):
+    stone_set, max_y = build_map(filename)
+    running = True
+    total_sand = 0
+    max_y += 2
+    floor_height = max_y
+    while running and SAND_START not in stone_set:
+        total_sand += 1
+        running, max_y = simulate_sand(stone_set, max_y, floor=floor_height)
+    return total_sand
+
+
+assert max_moves_void(TEST_INPUT) == 24
+print("Part One: ", max_moves_void(INPUT))
+assert max_moves_floor(TEST_INPUT) == 93
+print("Part Two: ", max_moves_floor(INPUT))
